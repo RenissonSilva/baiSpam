d7becc77636c8ec2e1caef18cc47bb19bd4c2e35
Renisson Silva <renissonx@gmail.com>
2023-03-30 01:29:11 -0300

    Adição das imagens e lógica para alterar todas as imagens do site visitado para um dos gifs

---
+++ images.js
@@ -0,0 +1,9 @@
+let imgs = document.getElementsByTagName('img')
+
+for(img of imgs) {
+    let r = Math.floor(Math.random() * 23)
+
+    let file = `images/bai-${r+1}.gif`
+    let url = chrome.extension.getURL(file)
+    img.src = url
+}
