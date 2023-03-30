e2ccedfd65022f80f9a78d1db1d3e0bd15a8ad02
Renisson Silva <renissonx@gmail.com>
2023-03-30 02:22:07 -0300

    Adicionando ícone na extensão, atualizando manifest pra v3 e criando arquivo básico de index do popup

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ for(img of imgs) {
     let r = Math.floor(Math.random() * 23)
 
     let file = `images/bai-${r+1}.gif`
-    let url = chrome.extension.getURL(file)
+    let url = chrome.runtime.getURL(file)
     img.src = url
 }
