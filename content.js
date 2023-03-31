3ef3c595246d93723287a064f1ee1135451b10c8
Renisson Silva <renissonx@gmail.com>
2023-03-31 00:19:40 -0300

    Retirando console.log das imagens

--- content.js
+++ content.js
@@ -2,12 +2,10 @@
   const siteImages = document.getElementsByTagName('img');
   
   chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log(request, sender, sendResponse);
-  
     if (request.task === 'changeImages') {
       for (let i = 0; i < siteImages.length; i++) {
         const r = Math.floor(Math.random() * 25);
-        console.log('r',r)
+
         let file = `images/bai-${r+1}.gif`
         let url = chrome.runtime.getURL(file)
         siteImages[i].src = url;
